8a7194a0521466d7699cbd1f84fe00227d1b3dd2
Lcs-93 <baillylucas04@gmail.com>
2025-07-30 14:27:33 +0200

    position bot

--- backend/server.js
+++ backend/server.js
@@ -10,16 +10,24 @@ app.get("/", (req, res) => {
   res.send("Bot-War API is running. Try /action");
 });
 
-app.get("/action", (req, res) => {
+app.post("/action", (req, res) => {
+  const gameState = req.body;
 
-  console.log("GET /action called");
-  console.log("req.query :", req.query);
-  console.log("req.body  :", req.body); // souvent vide
-  // Réponse par défaut
-  res.json({
-    move: "STAY",
-    action: "NONE"
-  });
+  const bot = gameState.bot; // ton bot
+  const grid = gameState.grid; // grille 5x5
+  const x = bot.position.x;
+  const y = bot.position.y;
+
+  // Chercher un diamant à proximité (diamonds = 1pt)
+  let move = "STAY";
+  let action = "NONE";
+
+  if (x > 0 && grid[y][x - 1] === "diamond") move = "LEFT";
+  else if (x < 4 && grid[y][x + 1] === "diamond") move = "RIGHT";
+  else if (y > 0 && grid[y - 1][x] === "diamond") move = "UP";
+  else if (y < 4 && grid[y + 1][x] === "diamond") move = "DOWN";
+
+  res.json({ move, action });
 });
 
 app.listen(port , () => console.log("Le serveur tourne sur le port " + port));
